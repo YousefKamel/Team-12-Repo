dc6167cbde9cfd4418c0556306e2632b76c05a27
abdelrahman nasser <abdelrahman nasser>
2018-11-28 19:12:18 +0200

    add sort function

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,7 +5,39 @@ import java.util.Scanner;
 public class MyClass
 {
 	public MyClass()
-	{	}
+	{	
+	}
+	/////////////////////////////////////////////////
+	public static boolean CheckSorted(int [] arr)
+    	{
+        boolean value = true;
+        int [] out =new int [arr.length];
+        for (int i=0;i<arr.length;i++)
+        {
+            out[i]=arr[i];
+        }
+        for (int i=0;i<out.length;i++)
+        {
+            for (int j=i+1;j<out.length;j++)
+            {
+                if(out[i]>out[j])
+                {
+                    int temp = out[i];
+                    out[i]=out[j];
+                    out[j]=out[i];
+                }
+            }
+        }
+        for (int i=0;i<arr.length;i++)
+        {
+            if (arr[i]!=out[i])
+            {
+                value=false;
+                break;
+            }
+        }
+        return value;
+    	}
 	/////////////////////////////////////////////////
 	public static boolean isPrimeNumber(int i)
 	{
